fa4674acbf1baa28275ca263e3799fb6c5e0bd01
priyankapiku1711 <priyankapiku1711@gmail.com>
2023-08-30 15:18:48 +0000

    any message

---
+++ ProjectAaBbCc/src/main/java/Projectaabbcc/ProjectAaBbCc/App.java
@@ -0,0 +1,13 @@
+package Projectaabbcc.ProjectAaBbCc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
